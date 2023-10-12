ef5733c707d4a795d9d6800589f9e469228fa1e5
Saad Shaikh <shksaad911@gmail.com>
2023-10-12 11:48:00 +0530

    added dark mode

--- app/components/DarkModeSwitch.jsx
+++ app/components/DarkModeSwitch.jsx
@@ -1,17 +1,38 @@
-"use client"
-import React from 'react';
-import { MdNightlight, MdLightMode } from 'react-icons/md';
+"use client";
+import React, { useEffect, useState } from "react";
+import { MdNightlight, MdLightMode } from "react-icons/md";
 
-import { useTheme } from 'next-themes';
+import { useTheme } from "next-themes";
 
 const DarkModeSwitch = () => {
-    const {systemTheme, theme, setTheme } = useTheme();
-    return (
-        <div>
-            <MdLightMode />
-            <MdNightlight />
-        </div>
-    );
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  useEffect(() => {
+    setMounted(true);
+
+    return () => {
+      setMounted(false);
+    };
+  }, []);
+  return (
+    <>
+      {mounted &&
+        (currentTheme === "dark" ? (
+          <MdLightMode
+            className="cursor-pointer hover:text-amber-500 hover:transition-all"
+            onClick={() => setTheme("light")}
+          />
+        ) : (
+          <MdNightlight
+            className="cursor-pointer hover:text-amber-500 hover:transition-all"
+            onClick={() => setTheme("dark")}
+          />
+        ))}
+    </>
+  );
 };
 
 export default DarkModeSwitch;
--- app/components/MenuItem.jsx
+++ app/components/MenuItem.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const MenuItem = ({title, address, Icon}) => {
   return (
     <>
-    <Link href={address} className='mx-4 lg:mx-6 text-gray-600 hover:text-amber-600'>
+    <Link href={address} className='mx-4 lg:mx-6 hover:text-amber-600'>
         <Icon className="text-2xl sm:hidden inline" />
         <p className='hidden sm:inline my-2 text-lg font-semibold'>{title}</p>
     </Link>
